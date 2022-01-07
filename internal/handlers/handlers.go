19349b69aebb75967f98ac99970521567e8b0b45
galifornia <martinho87@gmail.com>
2022-01-08 00:00:46 +0100

    Add websocket listeners & box for showing chat messages

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -26,11 +26,22 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
 // WsJsonResponse defines the response sent back from wesocket
 type WsJsonResponse struct {
 	Action      string `json:"action"`
 	Message     string `json:"message"`
 	MessageType string `json:"message_type"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
 }
 
 // WsEndpoint upgrade connection to websocket
