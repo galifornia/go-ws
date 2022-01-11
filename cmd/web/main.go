7b072a756b193f2bd2f7f0a3f6a0ed9f96b241b9
galifornia <martinho87@gmail.com>
2022-01-11 23:49:36 +0100

    Listen to Ws and broadcast messages to all

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/galifornia/go-ws/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting go routine that broadcasts messages")
+	go handlers.ListenWsChannel()
+
 	log.Println("Starting web server on port 8080")
 
 	_ = http.ListenAndServe(":8080", mux)
